7d5ca75264f2c3068a5cb68302f11d93e83140bc
Char <2855239858@qq.com>
2022-02-28 13:50:41 -0600

    2.28

--- test.cpp
+++ test.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
 
+#include <memory>
+#include <string>
+
 using namespace std;
 
+class test {
+public:
+    test() {
+
+    }
+};
+
+typedef shared_ptr <test> testPtr;
+
+int main() {
+    test *t1 = new test();
+    cout<<"original address: "<<t1<<endl;
+
+    testPtr t2 = make_shared<test>();
+    testPtr t3 = t2;
+
+    cout<<"t2: "<<t2<<"-t2 original address: "<<t2.get()<<endl;
+    cout<<"t3: "<<t3<<"-t3 original address: "<<t3.get()<<endl;
+
+    getchar();
+    return 0;
+}
